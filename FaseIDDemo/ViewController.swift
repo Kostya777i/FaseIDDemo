acc3906d47ac9876ecffb03b138f6ec8c8f9f07c
Kostya Losev <Kostya777i@gmail.com>
2022-10-30 09:46:27 +0500

    Demo

--- FaseIDDemo/ViewController.swift
+++ FaseIDDemo/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var statusLabel: UILabel!
+
+    @IBAction func authButtonPressed() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authorize with Face ID"
+            
+            context.evaluatePolicy(
+                .deviceOwnerAuthenticationWithBiometrics, localizedReason: reason
+            ) { success, error in
+                DispatchQueue.main.async {
+                    guard success, error == nil else {
+                        self.statusLabel.text = "Your Biometric Status: \n" + "Failed"
+                        self.statusLabel.textColor = .red
+                            self.shoeAlert(title: "Ошибка", message: "Попробуйте снова")
+                        return
+                    }
+                    
+                    self.statusLabel.text = "Your Biometric Status: \n" + "Logged In"
+                    self.statusLabel.textColor = .green
+                }
+            }
+        } else {
+            if let error {
+                shoeAlert(title: "Нет доступа", message: "\(error.localizedDescription)")
+            }
+        }
     }
-
-
 }
 
+extension ViewController {
+    private func shoeAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Отмена", style: .cancel)
+        alert.addAction(dismissAction)
+        present(alert, animated: true )
+    }
+}
